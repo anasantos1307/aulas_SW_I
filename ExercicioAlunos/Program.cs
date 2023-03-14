f92ea6928a62fe58834265f598a9039a2f829f4c
Ana Clara Martins dos Santos <100931641+anasantos1307@users.noreply.github.com>
2023-03-14 10:55:04 -0300

    Exercício feito com o desafio cumprido 14/03

---
+++ ExercicioAlunos/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ExercicioAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o nome do aluno: ");
+        Alunos aluno1 = new Alunos();
+        aluno1.nome = Console.ReadLine();
+        Console.WriteLine("Digite a primeira nota do aluno: ");
+        aluno1.nota1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a segunda nota do aluno: ");
+        aluno1.nota2 = double.Parse(Console.ReadLine());
+
+        aluno1.mensagem();
+    }
+}
