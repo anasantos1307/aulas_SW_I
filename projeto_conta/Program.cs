b5a9355b4aeb96914f0e06ae1ee3d56ca0223b12
Ana Clara Martins dos Santos <100931641+anasantos1307@users.noreply.github.com>
2023-03-21 11:09:00 -0300

    Aula 21/03

---
+++ projeto_conta/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace projeto_conta;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Banco Etec MCM!");
+        Conta c = new Conta();
+        //c.Saldo = 1000;
+        c.Depositar(1000);
+        c.Sacar(800);
+        //c.Limite = 500;
+        c.AjustarLimite(500);
+
+        double valorTotal = c.MostrarSaldo();
+
+        //Console.WriteLine("Seu saldo é de: " + c.Saldo + " com limite de: " + c.Limite);
+
+        Console.WriteLine("Seu saldo total é de " + valorTotal);
+        //Console.WriteLine("Seu saldo total é de " + c.Saldo);
+    }
+}
