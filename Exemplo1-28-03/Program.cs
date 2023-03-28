4fa622f7fe1ce672f27e0d9df461a8fcb0353e86
Ana Clara Martins dos Santos <100931641+anasantos1307@users.noreply.github.com>
2023-03-28 11:00:53 -0300

    Aula 28/03

---
+++ Exemplo1-28-03/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Exemplo1_28_03;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p1 = new Produto("Biro Biro", 10);
+        p1.MostraDados();
+    }
+}
---
+++ aula 28-03/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace aula_28_03;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("Ana",16);
+        //p1.InsereNome("Ana Clara");
+        //p1.InsereSobreNome("Martins");
+        //p1.InsereIdade(16);
+
+        p1.MostraDados();
+    }
+}
