ec01d6255dc74b694ebb9dd80d0ffb3074c66594
Ana Clara Martins dos Santos <100931641+anasantos1307@users.noreply.github.com>
2023-04-04 09:57:45 -0300

    Exercício da aula dia 28/03 realizado dia 04/04

---
+++ Exercicio-aviao-04_04/Aeroporto.cs
@@ -0,0 +1,44 @@
+﻿namespace Exercicio_aviao_04_04;
+class Aeroporto
+{
+   public Aeroporto(){
+    this.Codigo = 0;
+    this.Empresa = "Não Informado!";
+    this.Entrada = "Não Informado!";
+    this.Saida = "Não Informado!";
+   }
+   public Aeroporto(int codigo, string nome){
+    this.Codigo = 0;
+    this.Empresa = "Não Informado!";
+    this.Entrada = "Não Informado!";
+    this.Saida = "Não Informado!";
+   }
+
+   private int Codigo{get; set;}
+   private string? Empresa{get; set;}
+   private string? Entrada{get; set;}
+   private string? Saida{get; set;}
+
+   public void AlteraCodigo(int codigo){
+    this.Codigo = codigo;
+   }
+
+   public void AlteraEmpresa(string empresa){
+    this.Empresa = empresa;
+   }
+
+   public void AlteraEntrada(string entrada){
+    this.Entrada = entrada;
+   }
+
+   public void AlteraSaida(string saida){
+    this.Saida = saida;
+   }
+
+   public void MostraDados(){
+    Console.WriteLine("Código: " + this.Codigo);
+    Console.WriteLine("Empresa: " + this.Empresa);
+    Console.WriteLine("Entrada: " + this.Entrada);
+    Console.WriteLine("Saída: " + this.Saida);
+   }
+}
---
+++ Exercicio-aviao-04_04/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio_aviao_04_04;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aeroporto a1 = new Aeroporto();
+        a1.AlteraCodigo(1);
+        a1.AlteraEmpresa("Rajaram Airlines");
+        //a1.AlteraEntrada("Itália");
+        //a1.AlteraSaida("Dinamarca");
+        a1.MostraDados();
+    }
+}
